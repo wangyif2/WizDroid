2a0fdf51ae80efa99e77fa6441531bf8387e28ed
Robert Wang <ywang@Husky.corp.soti.net>
2012-07-21 18:52:13 -0400

    Modified to add OnAudioFocusChangeListener to gain back registerMediaButtonEventReceiver when audio focus is gone

--- src/com/yifandriod/wizdroid/Main.java
+++ src/com/yifandriod/wizdroid/Main.java
@@ -11,8 +11,7 @@ import com.google.inject.Injector;
 import com.yifandriod.wizdroid.WizLog.LoggingModule;
 import com.yifandriod.wizdroid.WizLog.WizLogger;
 
-public class Main extends Activity
-{
+public class Main extends Activity {
     @Inject
     private WizLogger wizLogger;
 
@@ -20,10 +19,11 @@ public class Main extends Activity
     private Context mContext;
     private ComponentName componentName;
 
-    /** Called when the activity is first created. */
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
@@ -37,12 +37,50 @@ public class Main extends Activity
 //        injector.injectMembers(this);
         wizLogger.info("We are in main onCreate");
 
+        AudioManager.OnAudioFocusChangeListener afChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+            @Override
+            public void onAudioFocusChange(int focusChange) {
+                wizLogger.info("AudioFocusChanged");
+                if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT) {
+                    wizLogger.info("AUDIOFOCUS_LOSS_TRANSIENT");
+//                    am.registerMediaButtonEventReceiver(componentName);
+                }
+                else if (focusChange == AudioManager.AUDIOFOCUS_GAIN){
+                    wizLogger.info("AUDIOFOCUS_GAIN");
+                    am.registerMediaButtonEventReceiver(componentName);
+                }
+                else if (focusChange == AudioManager.AUDIOFOCUS_LOSS){
+                    wizLogger.info("AUDIOFOCUS_LOSS");
+                    int result = am.requestAudioFocus(this,AudioManager.STREAM_MUSIC,AudioManager.AUDIOFOCUS_GAIN);
+                    wizLogger.info(result);
+                    am.registerMediaButtonEventReceiver(componentName);
+//                    am.abandonAudioFocus(afChangeListener);
+                    // Stop playback
+                }
+            }
+        };
+
+
+        // Request audio focus for playback
+
+        int result = am.requestAudioFocus(afChangeListener,AudioManager.STREAM_MUSIC,AudioManager.AUDIOFOCUS_GAIN);
+        if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED)
+        {
+            wizLogger.info("AUDIOFOCUS_REQUEST_GRANTED");
+//            am.unregisterMediaButtonEventReceiver(componentName);
+            // Start playback.
+        }
+
         am.registerMediaButtonEventReceiver(componentName);
     }
+
+
+
 
     @Override
     protected void onStop() {
         super.onStop();    //To change body of overridden methods use File | Settings | File Templates.
-        am.unregisterMediaButtonEventReceiver(componentName);
+        wizLogger.info("unregisterMediaButtonEventReceiver onStop");
+//        am.unregisterMediaButtonEventReceiver(componentName);
     }
 }
